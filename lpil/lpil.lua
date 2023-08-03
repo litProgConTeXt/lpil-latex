c036b8ca53e97d2d0fff271a684deff2de6acfbf
Stephen Gaito <stephen@perceptisys.co.uk>
2023-08-03 15:52:07 +0100

    added including lpil diagrams from the latex directory

--- lpil/lpil.lua
+++ lpil/lpil.lua
@@ -2,8 +2,77 @@
 local lpilMod = {}
 
 local tInsert = table.insert
+local tRemove = table.remove
 local tConcat = table.concat
+local sSub    = string.sub
+local sGSub   = string.gsub
 local sFormat = string.format
+
+-------------------------------------------------------------------------
+-- initialize this module
+
+function lpilMod.initialize()
+  local homeDir = os.getenv("HOME")
+  lpilMod.config = dofile(homeDir..'/.config/cfdoit/config.lua')
+
+  if (lpilMod.config['build']) then
+    if (lpilMod.config['build']['latexDir']) then
+      local latexDir = lpilMod.config['build']['latexDir']
+      texio.write("\nSetting LaTeXDir = "..latexDir.."\n")
+      tex.print("\\def\\latexBuildDir{"..latexDir.."}")
+    else 
+      tex.print("\\def\\latexBuildDir{.}")
+    end
+  else
+    tex.print("\\def\\latexBuildDir{.}")
+  end
+end
+
+-------------------------------------------------------------------------
+-- Provide a simple stack of "input files"
+
+local function sEndsWith(aStr, aSuffix)
+  return sSub(aStr, -#aSuffix) == aSuffix
+end
+
+local inputFiles = {}
+
+function lpilMod.topInputFile()
+  inputFile = "unknown"
+  if 0 < #inputFiles then
+    inputFile = inputFiles[#inputFiles]
+  end
+  --texio.write("\ncurrentInputFile: "..inputFile.."\n")
+  return inputFile
+
+end
+
+function lpilMod.currentInputFile()
+  tex.print(lpilMod.topInputFile)
+end
+
+function lpilMod.pushInputFile(aPath)
+  --lpilMod.topInputFile()
+
+  -- ensure we honour the LaTeX \input/\include by silently adding the
+  -- .tex if there is no file extension...
+
+  if (sEndsWith(aPath, '.tex') or sEndsWith(aPath, '.sty')) then
+    -- do nothing
+  else
+    aPath = aPath..'.tex'
+  end
+
+  --texio.write("\npushInputFile: "..aPath.."\n")
+  tInsert(inputFiles, aPath)
+end
+
+function lpilMod.popInputFile()
+  --texio.write("\npopInputFile\n")
+  tRemove(inputFiles, aPath)
+  --lpilMod.topInputFile()
+end
+-------------------------------------------------------------------------
 
 -- Note the ordering of the "extra" \\begingroup / \\endgroup pair...
 --
@@ -30,21 +99,33 @@ end
 local fileCounters = {}
 
 function lpilMod.defineLoadPygmentedCode(codeType, baseName)
-  texio.write("\ndefineLoadPygmentedCode("..codeType..","..baseName..")\n")
+  texio.write("\ndefineLoadPygmentedCode("..codeType..","..baseName..")")
+
+  curFilePath = lpilMod.topInputFile()
+  --texio.write("\ncurFilePath: "..curFilePath.."\n")
+  -- need to change directory separators to a simple '.'
+  curFilePath = sGSub(curFilePath, '[%\\%/]', '.')
+  --texio.write("\ncurFilePath: "..curFilePath.."\n")
 
   fileCounters[codeType] = fileCounters[codeType] or {}
-  if fileCounters[codeType][baseName] == nil then
-    fileCounters[codeType][baseName] = 1
+  codeTypes = fileCounters[codeType]
+  codeTypes[curFilePath] = codeTypes[curFilePath] or {}
+  curFile = codeTypes[curFilePath]
+  if curFile[baseName] == nil then
+    curFile[baseName] = 1
   else
-    fileCounters[codeType][baseName] = fileCounters[codeType][baseName] + 1
+    curFile[baseName] = curFile[baseName] + 1
   end
 
   fileName = {}
-  tInsert(fileName, "c")
-  tInsert(fileName, sFormat("%05d", fileCounters[codeType][baseName]))
   tInsert(fileName, baseName)
+  tInsert(fileName, ".")
+  tInsert(fileName, curFilePath)
+  tInsert(fileName, ".c")
+  tInsert(fileName, sFormat("%05d", curFile[baseName]))
   tInsert(fileName, ".pygmented.tex")
   fileName = tConcat(fileName, '')
+  texio.write("\n  will load file: "..fileName.."\n")
 
   texCmd = {}
   tInsert(texCmd, "\\def\\loadPygmentedCode{\\IfFileExists{")
@@ -54,7 +135,7 @@ function lpilMod.defineLoadPygmentedCode(codeType, baseName)
   tInsert(texCmd, "}}{\\par \\noindent \\fbox{ Pygmented ")
   tInsert(texCmd, codeType)
   tInsert(texCmd, " code for chunk ")
-  tInsert(texCmd, sFormat("%d", fileCounters[codeType][baseName]))
+  tInsert(texCmd, sFormat("%d", curFile[baseName]))
   tInsert(texCmd, " of ")
   tInsert(texCmd, baseName)
   tInsert(texCmd, " does not exist} \\par }}")
